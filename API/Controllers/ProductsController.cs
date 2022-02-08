f5d9303bf1bd7b716b341b1a991d6ff19487b198
nicjh77 <nicjh77@gmail.com>
2022-02-08 11:40:01 -0500

    End of section 5

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -8,9 +8,8 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class ProductsController : ControllerBase
+    
+    public class ProductsController : BaseApiController
     {
         private readonly StoreContext _context;
 
@@ -22,14 +21,17 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            return await _context.Products.ToListAsync();
-            
+            return await _context.Products.ToListAsync();    
         }
 
         [HttpGet("{id}")]  // api/products/3
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return await _context.Products.FindAsync(id);
+            var product = await _context.Products.FindAsync(id);
+
+            if(product == null) return NotFound();
+
+            return product;
         }
     }
 }
--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -2,13 +2,18 @@ import { ThemeProvider } from "@emotion/react";
 import { Container, createTheme } from "@mui/material";
 import CssBaseline from '@mui/material/CssBaseline';
 import { useState } from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import AboutPage from "../../features/about/AboutPage";
 import Catalog from "../../features/catalog/Catalog";
 import ProductDetails from "../../features/catalog/ProductDetails";
 import ContactPage from "../../features/contact/ContactPage";
 import HomePage from "../../features/home/HomePage";
 import Header from "./Header";
+import 'react-toastify/dist/ReactToastify.css';
+import ServerError from "../errors/ServerError";
+import NotFound from "../errors/NotFound";
+
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -27,14 +32,19 @@ function App() {
   }
   return (
     <ThemeProvider theme={theme}>
+      <ToastContainer position='bottom-right' hideProgressBar />
       <CssBaseline />
       <Header darkMode={darkMode} handleThemeChange = {handleThemeChange} />
       <Container>
+        <Switch>
         <Route path='/' component={HomePage} exact />
         <Route path='/catalog' component={Catalog} exact />
         <Route path='/catalog/:id' component={ProductDetails} />
         <Route path='/about' component={AboutPage} />
         <Route path='/contact' component={ContactPage} />
+        <Route path='/server-error' component={ServerError} />
+        <Route component={NotFound} />
+        </Switch>  
       </Container>
             
     </ThemeProvider>
